4c9817250346293f0cac54fa1e0e3099a15ed4d5
Yawkiii <hamzachamsi70@gmail.com>
2023-04-18 22:04:17 -0400

    Ajout de la classe client

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AppHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Client
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Courriel { get; set; }
+        public string CarteDeCredit { get; set; }
+        public int NumeroChambre { get; set; }
+
+        public Client(string nom, string prenom, string courriel, string carteDeCredit, int numeroChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteDeCredit = carteDeCredit;
+            NumeroChambre = numeroChambre;
+        }
+    }
+}
