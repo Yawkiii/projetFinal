397dbdca0e7690cd413de2ccd0e20a83e4ac8013
Yawkiii <hamzachamsi70@gmail.com>
2023-04-18 22:06:58 -0400

    Ajout de la classe hotel

--- Program.cs
+++ Program.cs
@@ -24,4 +24,18 @@
             NumeroChambre = numeroChambre;
         }
     }
-}
+    public class Hotel
+    {
+        private List<Client> Clients { get; set; }
+
+        public Hotel()
+        {
+            Clients = new List<Client>();
+        }
+
+        public void AjouterClient(Client client)
+        {
+            Clients.Add(client);
+        }
+
+    }
